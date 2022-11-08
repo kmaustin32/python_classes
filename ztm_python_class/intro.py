6c4ddb702bb64dccfbd698156de32a7f15729323
Keenan (עזרא) <reaper31292@gmail.com>
2022-11-08 12:26:43 +0200

    Windows branch1 (#2)
    * intro to class 1 11.8.22

---
+++ ztm_python_class/intro.py
@@ -0,0 +1,53 @@
+# user_name = input("What is your name? >> ")
+# print(f"Hello {user_name}!")
+
+# Math Functions
+
+print(round(3.23))
+print(abs(-20))
+
+# Divide and Round
+print(7 // 3)
+
+# Complex, a third type after Int and Float
+# Bin will turn into a binary
+print(bin(5))
+# Turn that binary into a base 2 integer
+print(int('0b101', 2))
+
+# Variables in Python don't have a var, const, let designation
+# Snake_case for normal variables, CAPS for constants 
+# __ is called a dunder, aka a double under. These are keywords.
+
+my_name = "Ezra"
+PI = 3.14
+
+# You can define multiple variables at once.
+
+a,b,c = 1, 2, 3
+
+# Expression is a piece of code that produces a value (right side)
+# A statement is an entire line of code (whole thing)
+
+user_age = 80 / PI
+print(round(user_age, 2))
+
+# Augmented assignment operator 
+some_value = 5
+some_value = some_value + 2
+
+# or instead, we can use augmented assignment
+some_value += 3
+
+# Strings...
+'This is a string, like it is in most languages...'
+"This is also a string..."
+'''
+And this is
+a long
+string...
+'''
+
+# You can use + to concat. first_name + " " + last_name
+
+
